14668ecf83e7b39e9309761c97aa0336322d3790
bouzaHichem <116437633+bouzaHichem@users.noreply.github.com>
2025-08-31 19:55:54 +0100

    feat(admin mobile): add hamburger in Navbar to toggle sidebar; enable mobile sidebar overlay

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,11 +3,23 @@ import './Navbar.css'
 import navlogo from '../Assets/ltogod.JPG'
 import navprofileIcon from '../Assets/nav-profile.svg'
 
-const Navbar = () => {
+const Navbar = ({ onMobileMenuToggle }) => {
   return (
     <div className='navbar'>
-      <img src={navlogo} className='nav-logo' alt="" />
-      <img src={navprofileIcon} className='nav-profile' alt="" />
+      <div className='nav-left'>
+        {/* Mobile hamburger */}
+        <button 
+          className='nav-hamburger' 
+          aria-label='Open menu' 
+          onClick={onMobileMenuToggle}
+        >
+          <span className='bar' />
+          <span className='bar' />
+          <span className='bar' />
+        </button>
+        <img src={navlogo} className='nav-logo' alt="Damio Kids" />
+      </div>
+      <img src={navprofileIcon} className='nav-profile' alt="Profile" />
     </div>
   )
 }
